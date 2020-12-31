b7f508b42a7c9920c62ee8857f0e9e1e90638144
Ronnir-Leon76 <ronleon76@gmail.com>
2020-12-31 16:59:19 +0300

    On branch master

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn my_django_app.wsgi --log.file -
--- my_django_app/settings.py
+++ my_django_app/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'g@l-03c@!q$!cl7^(6+hb+y#fd(j^ojyf2toc*6-nln#4@6@cy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['subaruenthusiasts.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
